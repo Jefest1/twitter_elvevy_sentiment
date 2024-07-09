054d7b271baa056ea1e3c45532c17826369b7620
Jefest1 <anyanf101@gmail.com>
2024-07-09 16:19:17 +0000

    creating scripts

---
+++ config/config.py
@@ -0,0 +1,9 @@
+# config.py
+
+client_ID = "WlZLbGp2N0hGRi11UjFRak1VcHc6MTpjaQ"
+client_Secret = "28wwjkCmiklzmSsXbEsLmlbWVINN0dqN4uC7-IvALSSoD6jIyX"
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAHL4ugEAAAAAYNiKeIeGc38ZBXrupnZsogDSRCc%3DmzNWPhYhI6QHDToFJWY97ALwBBR1bsurV84oqSmBdCVwMJGPBP"
+access_token = "AAAAAAAAAAAAAAAAAAAAAHL4ugEAAAAAYNiKeIeGc38ZBXrupnZsogDSRCc%3DmzNWPhYhI6QHDToFJWY97ALwBBR1bsurV84oqSmBdCVwMJGPBP"
+access_token_secret = "mkMAQN1eWGgoSQ3DEA8iv1J0uNCoYmBIUcUVeEmCDbHF0"
+api_key = "tPp6BJtRwf9GcP9QPMfT5JJcE"
+api_key_secret = "gA4vr2pWKHHIjGr8VmHgX6SjtZu96kGKxwAqlvXVvmj67oRnmf"
